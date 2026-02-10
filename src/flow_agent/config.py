bd3855049905d3a17525b55cc9fc3b25682b5779
AmritShankarDutta <amrit08ju@gmail.com>
2026-02-10 13:29:41 +0530

    refactoring, multi turn . image retain . summarization

--- src/flow_agent/config.py
+++ src/flow_agent/config.py
@@ -76,7 +76,7 @@ class Settings(BaseSettings):
     IS_PII_REDACTION_ENABLED: bool = False
 
     # Summarization Settings
-    SUMMARY_MESSAGE_THRESHOLD: int = 3  # Trigger after N messages
+    SUMMARY_MESSAGE_THRESHOLD: int = 10  # Trigger after N messages
     SUMMARY_PROVIDER_PREFERENCE: str = 'langchain'  # 'langchain' or 'genai'
 
 
